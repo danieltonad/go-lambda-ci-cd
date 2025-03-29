e39b2b4ee6da592b463c1f80f1f18cdb5010e95f
Daniel Solarin <soladan2000@gmail.com>
2025-03-29 04:33:14 -0900

    Add AWS Lambda function and deployment workflow

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lambdaCICD
 
 go 1.23.2
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+	response := events.LambdaFunctionURLResponse{
+		Body:       "Hello, World!",
+		StatusCode: 200,
+	}
+
+	return response, nil
+}
