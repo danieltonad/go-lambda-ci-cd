2a84a8b709aef5b6c46fde62ccbe25e323eb9be3
Daniel Solarin <soladan2000@gmail.com>
2025-03-30 18:51:44 -0900

    Add route handling to Lambda function and update .gitignore

--- main.go
+++ main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
-	lambda.Start(handler)
+// Lambda handler
+func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+	fmt.Println("Received request:", request.RequestContext.HTTP.Method, request.RawPath)
+
+	switch request.RawPath {
+	case "/":
+		return homeHandler()
+	case "/hello":
+		return helloHandler()
+	case "/bye":
+		return byeHandler()
+	default:
+		return notFoundHandler()
+	}
 }
 
-func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
-	fmt.Print("Func Exec")
-	response := events.LambdaFunctionURLResponse{
-		Body:       "Hello, World!",
+// Handlers for different routes
+func homeHandler() (events.LambdaFunctionURLResponse, error) {
+	return events.LambdaFunctionURLResponse{
 		StatusCode: 200,
-	}
+		Body:       "Welcome to the Home Page!",
+	}, nil
+}
 
-	return response, nil
+func helloHandler() (events.LambdaFunctionURLResponse, error) {
+	return events.LambdaFunctionURLResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
+}
+
+func byeHandler() (events.LambdaFunctionURLResponse, error) {
+	return events.LambdaFunctionURLResponse{
+		StatusCode: 200,
+		Body:       "Goodbye!",
+	}, nil
+}
+
+func notFoundHandler() (events.LambdaFunctionURLResponse, error) {
+	return events.LambdaFunctionURLResponse{
+		StatusCode: http.StatusNotFound,
+		Body:       "404 Not Found",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
